20eb2ea0125a43014e377393fee0234b42b05e26
shashankyo <shashank944533@gmail.com>
2023-03-26 01:23:46 +0530

    initial commit sidebar

---
+++ app.js
@@ -0,0 +1,20 @@
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-Btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click",function() {
+    console.log(sidebar.classList);
+    //if(sidebar.classList.contains("show-sidebar")) {
+       // sidebar.classList.remove("show-sidebar");
+   // }
+   // else {
+    //    sidebar.classList.add("shoe-sidebar");
+   // }
+    sidebar.classList.toggle("shoebar");
+});
+
+
+closeBtn.addEventListener("click",function() {
+    sidebar.classList.remove("show-sidebar");
+
+});
